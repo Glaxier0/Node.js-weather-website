fb66dfaaae2d6feae84a519f4d2d9a3f534888c9
Tahir <55876415+Glaxier0@users.noreply.github.com>
2021-10-24 21:25:38 +0300

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require("postman-request")
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token={your_access_token_here}&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=4790e641db6e541f57e22fea82ca9ab5&limit=1'
 
     request({url, json: true}, (error, {body}) => {
         if (error) {
