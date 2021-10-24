e18c12f915ddb65e3da08c4d16baab6f4304bfb5
Tahir <55876415+Glaxier0@users.noreply.github.com>
2021-10-24 21:14:49 +0300

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("postman-request")
 
 const forecast = (latitude, longitude,callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=4790e641db6e541f57e22fea82ca9ab5&query=' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key={your_acces_key_here}&query=' + latitude + ',' + longitude
 
     request({url, json: true}, (error, {body}) => {
         if (error) {
