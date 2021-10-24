98012622d11597018a4a52ebfd9c4e74a4364a32
glaxier0 <tahir2001@hotmail.com>
2021-10-24 20:18:42 +0300

    Added new line to forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,8 @@ const forecast = (latitude, longitude,callback) => {
             callback('Unable to find location.', undefined)
         } else {
             const currentData = body["current"]
-            callback(undefined, currentData["weather_descriptions"][0] + ', degree: ' + currentData["temperature"]
-                + '°C feels like: ' + currentData["feelslike"] + '°C Humidity: ' + currentData["humidity"] + '%')
+            callback(undefined, currentData["weather_descriptions"][0] + ', Degree: ' + currentData["temperature"]
+                + '°C Feels like: ' + currentData["feelslike"] + '°C Humidity: ' + currentData["humidity"] + '%')
         }
     })
 }
