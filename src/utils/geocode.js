b22b222a674d78591e1f6337131d44989d588f60
glaxier0 <tahir2001@hotmail.com>
2022-04-08 22:45:41 +0300

    Docker init

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require("postman-request")
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZ2xheGllciIsImEiOiJja3V3cHg3a24yN2tsMnVxdnRrd3Fmb3RkIn0.wxgZEGYNweL18p9MWRASkw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token={your_access_token}&limit=1'
 
     request({url, json: true}, (error, {body}) => {
         if (error) {
